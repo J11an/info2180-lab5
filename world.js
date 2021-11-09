8e2932e55a89e122184de518dc8687b02161dc52
J.Wan <jaysun1w@gmail.com>
2021-11-09 13:28:56 -0500

    Ex 4 completed

--- world.js
+++ world.js
@@ -1,25 +1,25 @@
-window.onload  = function(){
+window.onload = function() {
 
     let btn = document.querySelector("#lookup");
-    let text = document.querySelector("#country").value;
+    let filler  = document.querySelector("#country");
     let result = document.querySelector("#result");
 
     btn.addEventListener("click", function(e) {
+
         e.preventDefault();
-        fetch("world.php?query="+text.value)
+        fetch("world.php?country="+filler.value)
+
             .then(response => {
                 if (response.ok) {
                     return response.text()
                 } else {
-                    return Promise.reject("Error")
+                    return Promise.reject("Error!")
                 }
             })
             .then(data => {
                 result.innerHTML = data;
             })
-            .catch(error => console.log("There was an error: " + error));
-        
-        });
-    }
+            .catch(error => console.log(error));
 
-    
+    });
+} 
--- world.php
+++ world.php
@@ -3,16 +3,34 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
-$country = $_GET['country'];
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$s_country = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$s_result = $s_country->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence Year</th>
+    <th>Head of State</th>
+  </tr>
+
+  <?php foreach ($s_result as $row): ?>
+<tr>
+  <td><?php echo $row['name']; ?></td>
+  <td><?php echo $row['continent']; ?></td>
+  <td><?php echo $row['independence_year']; ?></td>
+  <td><?php echo $row['head_of_state']; ?></td>
+</tr>
+
 <?php endforeach; ?>
-</ul>
+  
+
+
+
